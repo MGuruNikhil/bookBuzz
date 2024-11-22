e5134704963f68456f6d1b4502ae96391a11863b
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-11-23 00:33:13 +0530

    config changed

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = "https://.vercel.app/";
-export const apiUrl = "http://127.0.0.1:5555/";
+export const apiUrl = "https://bookbuzzapi.vercel.app/";
+// export const apiUrl = "http://127.0.0.1:5555/";
