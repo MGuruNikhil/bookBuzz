facd6d914b48376497af6d42ee635ca0a96d2502
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-11-23 10:44:27 +0530

    pg problem

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,9 @@
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectModule: pg,
     dialectOptions: {
         ssl: {
             require: true,
@@ -10,11 +12,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
 });
 
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
 
 export default sequelize;
